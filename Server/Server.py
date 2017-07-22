5e7adc3dc20e5a4bfdd25c8e2deee58b9741625c
houfeng-invest <398818703@qq.com>
2017-07-22 09:52:06 +0800

    study flask

--- Server/Server.py
+++ Server/Server.py
@@ -1,8 +1,21 @@
 from flask import Flask
+from flask_login import  LoginManager
 import flask_restful
 from  flask_restful import Resource
+
+
+
 app = Flask(__name__)
+from app import  views
+
 api = flask_restful.Api(app)
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+
 
 class Test(Resource):
     def get(self):
@@ -13,6 +26,9 @@ api.add_resource(Test,"/test")
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+
 
 
 if __name__ == '__main__':
